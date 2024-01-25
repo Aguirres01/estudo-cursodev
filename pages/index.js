ad16e336ef7417d2ac5111b93a5a7762954a39dc
Marlon Aguirre <91228732+Aguirres01@users.noreply.github.com>
2024-01-25 22:27:24 +0000

    tentativa de imagem

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,20 @@
 function Home (){
 
     return <h1> Oi delícia! Que pena que vc vai demorar. Vou ver se consigo fazer a janta antes de vc ir pro culto, Te amo!!!</h1>
+    function adicionarImagem() {
+        // Crie um elemento de imagem
+        var imagemElement = document.createElement('img');
+        
+        // Defina o caminho da imagem
+        imagemElement.src = 'https://unsplash.com/pt-br/t/street-photography';
+
+        // Adicione atributos adicionais, se necessário
+        imagemElement.alt = 'Descrição da Imagem';
+
+        // Adicione a imagem ao contêiner desejado no DOM
+        var imagemContainer = document.getElementById('imagemContainer');
+        imagemContainer.appendChild(imagemElement);
+    }
 
 }
 
